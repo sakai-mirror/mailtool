4f7973525812981d89e3ebe9cd27f1d505469414
kimsooil@bu.edu <kimsooil@bu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-11-09 16:24:03 +0000

    Added items remotely
    C:\sakai-2.2-1\2.2.1_AutoConfig
    C:\sakai-2.2-1\bumailtool5_1_provisional
    
    
    git-svn-id: https://source.sakaiproject.org/svn/mailtool/tags/2.2.1@25817 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ bumailtool5_1_provisional/src/java/org/sakaiproject/tool/mailtool/FoothillSelector.java
@@ -0,0 +1,271 @@
+package org.sakaiproject.tool.mailtool;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
+
+public class FoothillSelector implements RecipientSelector {
+	List /* EmailGroup */ m_initialgroups = null;
+	int m_poscount = 0;
+	
+	public class ListboxEntry
+	{
+		int m_position;
+		EmailUser m_user = null;
+		String m_label = "";
+		boolean m_selected = false;
+		
+		public ListboxEntry(int position, EmailUser user)
+		{
+			m_position = position;
+			m_user = user;
+			m_label = user.getDisplayname();
+		}
+		
+		public int getPosition()
+		{
+			return m_position;
+		}
+		
+		public String getLabel()
+		{
+			return m_label;
+		}
+		
+		public EmailUser getEmailUser()
+		{
+			return m_user;
+		}
+		
+		public void setSelected(boolean value)
+		{
+			m_selected = value;
+		}
+		
+		public boolean isSelected()
+		{
+			return m_selected;
+		}
+	}
+	
+	
+	public class ListboxGroup
+	{
+		String m_label = "";
+		List /* ListboxEntry */ m_entries = new ArrayList();
+		EmailGroup m_emailgroup = null;
+		
+		public String getLabel() { return m_label; }
+		public EmailGroup getEmailGroup() { return m_emailgroup; }
+		public List /* ListboxEntry */ getListboxEntries() { return m_entries; }
+		
+		
+		public ListboxGroup(EmailGroup group)
+		{
+			m_emailgroup = group;
+			m_label = group.getRolePlural();
+			
+			for (Iterator i = group.getEmailusers().iterator(); i.hasNext();)
+			{
+				EmailUser user = (EmailUser) i.next();
+				ListboxEntry entry = new ListboxEntry(m_poscount, user);
+				m_entries.add(entry);
+				m_poscount++;
+			}
+		}
+	}
+	
+	
+	/* This is going to be a List of Lists of type ListboxGroup */
+	List m_listboxGroups = new ArrayList();
+	
+	public FoothillSelector()
+	{
+		
+	}
+	
+	public void populate(List emailgroups) {
+		m_initialgroups = emailgroups;
+		m_listboxGroups.clear();
+		
+		for (Iterator i = emailgroups.iterator(); i.hasNext();)
+		{
+			EmailGroup group = (EmailGroup) i.next();
+			ListboxGroup listgroup = new ListboxGroup(group);
+			m_listboxGroups.add(listgroup);
+		}
+		
+	}
+
+	public List /* EmailUser */ getSelectedUsers() 
+	{
+		List users = new ArrayList();
+		
+		for (Iterator i = this.m_listboxGroups.iterator(); i.hasNext();)
+		{
+			ListboxGroup group = (ListboxGroup) i.next();
+			for (Iterator k = group.getListboxEntries().iterator(); k.hasNext();)
+			{
+				ListboxEntry entry = (ListboxEntry) k.next();
+				if (entry.isSelected() == true)
+				{
+					users.add(entry.getEmailUser());
+				}
+			}
+		}
+		
+		
+		return users;
+	}
+
+	public List getSelectedUsersByGroup() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** Methods for JSP Fragment **/
+	public List /* SelectItemGroup */ getListbox()
+	{
+		List selectItemGroups = new ArrayList();
+		
+		for (Iterator i = this.m_listboxGroups.iterator(); i.hasNext();)
+		{
+			List itemsingroup = new ArrayList();
+			ListboxGroup group = (ListboxGroup) i.next();
+			
+			SelectItemGroup siGroup = new SelectItemGroup();
+			siGroup.setValue(group.getLabel()); siGroup.setLabel(group.getLabel());
+			
+			SelectItem[] /* SelectItems */ selectitems = new SelectItem[group.getListboxEntries().size()];
+			
+			for (int j = 0; j < group.getListboxEntries().size(); j++)
+			{
+				ListboxEntry entry = (ListboxEntry) group.getListboxEntries().get(j);
+				SelectItem item = new SelectItem();
+				item.setLabel(entry.getLabel());
+				item.setValue(Integer.toString(entry.getPosition()));
+				selectitems[j] = item;
+				
+			}
+			
+			siGroup.setSelectItems(selectitems);
+			
+			selectItemGroups.add(siGroup);
+		}
+		
+		
+		return selectItemGroups;
+	}
+
+	public List /* String */ getSelectedItems()
+	{
+		List items = new ArrayList();
+		
+		for (Iterator i = m_listboxGroups.iterator(); i.hasNext();)
+		{
+			ListboxGroup group = (ListboxGroup) i.next();
+			for (Iterator j = group.getListboxEntries().iterator(); j.hasNext();)
+			{
+				ListboxEntry entry = (ListboxEntry) j.next();
+				if (entry.isSelected() == true)
+				{
+					items.add(Integer.toString(entry.getPosition()));
+				}
+			}
+		}
+		
+		return items;
+	}
+	
+	public void setSelectedItems(List /* ? */ items)
+	{
+		
+		Iterator i = items.iterator();
+		String next;
+		if (i.hasNext() == true)
+			next = (String) i.next();
+		else
+			return;
+		
+		for (Iterator j = this.m_listboxGroups.iterator(); j.hasNext();)
+		{
+			ListboxGroup group = (ListboxGroup) j.next();
+			for (Iterator k = group.getListboxEntries().iterator(); k.hasNext();)
+			{
+				ListboxEntry entry = (ListboxEntry) k.next();
+				if (next.equals(Integer.toString(entry.getPosition())))
+				{
+					entry.setSelected(true);
+					if (i.hasNext() == true)
+						next = (String) i.next();
+					else
+						return;
+				}
+				else
+					entry.setSelected(false);
+				
+			}
+		}
+		
+	}
+	
+	
+	public List /* String */ getCheckedRoles()
+	{
+		return new ArrayList();
+	}
+	
+	public void setCheckedRoles(List /* ? */ items)
+	{
+	
+	}
+	
+	public List /* SelectItem */ getRolesChecklist()
+	{
+		List selectItems = new ArrayList();
+		
+		for (Iterator i = this.m_initialgroups.iterator(); i.hasNext();)
+		{
+			EmailGroup e = (EmailGroup) i.next();
+			SelectItem item = new SelectItem();
+			item.setLabel(e.getRolePlural());
+			selectItems.add(item);
+		}
+		
+		return selectItems;
+	}
+	
+	
+	public String processSelectAll()
+	{
+		for (Iterator i = this.m_listboxGroups.iterator(); i.hasNext();)
+		{
+			ListboxGroup group = (ListboxGroup) i.next();
+			for (Iterator j = group.getListboxEntries().iterator(); j.hasNext();)
+			{
+				ListboxEntry entry = (ListboxEntry) j.next();
+				entry.setSelected(true);
+			}
+		}
+		
+		return "main";
+	}
+	
+	public String processSelectNone()
+	{
+		for (Iterator i = this.m_listboxGroups.iterator(); i.hasNext();)
+		{
+			ListboxGroup group = (ListboxGroup) i.next();
+			for (Iterator j = group.getListboxEntries().iterator(); j.hasNext();)
+			{
+				ListboxEntry entry = (ListboxEntry) j.next();
+				entry.setSelected(false);
+			}
+		}
+		
+		return "main";
+	}
+}
---
+++ bumailtool5_1_provisional/src/webapp/mailtool/main_onepage.jsp
@@ -0,0 +1,125 @@
+<%-- HTML JSF tag libary --%>
+<%@ taglib uri="http://java.sun.com/jsf/html" prefix="h"%>
+<%-- Core JSF tag library --%>
+<%@ taglib uri="http://java.sun.com/jsf/core" prefix="f"%>
+<%-- Main Sakai tag library --%>
+<%@ taglib uri="http://sakaiproject.org/jsf/sakai" prefix="sakai"%>
+
+<f:view>
+<f:loadBundle basename="org.sakaiproject.tool.mailtool.Messages" var="msgs" />
+
+<f:verbatim>
+<!DOCTYPE html
+ PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+</f:verbatim>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Mailtool</title>
+<%= request.getAttribute("sakai.html.head.css.base") %>
+<%= request.getAttribute("sakai.html.head.css.skin") %>
+<%= request.getAttribute("sakai.html.head.js") %>
+</head>
+<body onload="<%=request.getAttribute("sakai.html.body.onload")%>;">
+
+<div style="margin-left: 10px; margin-right: 10px">
+<h:form id="mainForm" enctype="multipart/form-data">
+
+<sakai:view_content>
+<sakai:messages />
+<h:panelGroup rendered="#{not Mailtool.allowedToSend}" >
+<f:verbatim><br/></f:verbatim>
+<h:outputText escape="false" value="#{msgs.no_mail_permission}<br/>" />
+</h:panelGroup>
+<h:panelGroup rendered="#{Mailtool.allowedToSend}" >
+<f:verbatim><br/></f:verbatim>
+<h:outputText escape="false" value="#{msgs.send_mail_to} " />
+					<h:selectOneListbox onchange="submit(); return false;" size="1" id="viewChoice" value="#{Mailtool.viewChoice}">
+						<f:selectItems value="#{Mailtool.viewChoiceDropdown}" />
+					</h:selectOneListbox>
+
+					<f:subview id="selectByRole" rendered="#{Mailtool.selectByRole}">
+						<jsp:include page="selectByRole.jsp" />
+					</f:subview>
+
+					<f:subview id="selectByUser" rendered="#{Mailtool.selectByUser}">
+						<jsp:include page="selectByUser.jsp" />
+					</f:subview>
+
+					<f:subview id="selectByTree" rendered="#{Mailtool.selectByTree}">
+						<jsp:include page="selectByTree.jsp" />
+					</f:subview>
+
+					<f:subview id="selectSideBySide" rendered="#{Mailtool.selectSideBySide}">
+						<jsp:include page="selectSideBySide.jsp" />
+					</f:subview>
+
+					<f:subview id="selectByFoothill" rendered="#{Mailtool.selectByFoothill}">
+						<jsp:include page="selectByFoothill.jsp" />
+					</f:subview>
+</h:panelGroup>
+<BR/>
+<h:panelGroup  rendered="#{Mailtool.allowedToSend}" >
+<h:outputText value="#{msgs.message_subject} "/>
+<h:inputText value="#{Mailtool.messageSubject}"	size="50"/>
+</h:panelGroup>
+<BR/>
+
+<h:panelGrid  rendered="#{Mailtool.attachClicked}" columns="7" border="0" cellspacing="5" cellpadding="0">
+<h:graphicImage alt="blank" url="/images/blank.gif" width="18" height="15"></h:graphicImage>
+<h:graphicImage alt="attachment_img" url="/images/paperclip.gif"  width="15" height="15"></h:graphicImage>
+<sakai:inputFileUpload size="50" valueChangeListener="#{Mailtool.processFileUpload}" />
+<h:commandButton value="Attach" type="submit"  action="#{Mailtool.toggle_attachClicked}" />
+<h:outputText value=" (max: " /><h:outputText value="#{msgs.max_upload_size}" /><h:outputText value=" MB)" />
+</h:panelGrid>
+
+<h:panelGroup rendered="#{not Mailtool.attachClicked and Mailtool.maxNumAttachment!=0}">
+<f:verbatim><br/></f:verbatim>
+<h:graphicImage alt="blank" url="/images/blank.gif" width="26" height="15"></h:graphicImage>
+<h:graphicImage alt="attachment_img" url="/images/paperclip.gif"  width="15" height="15"></h:graphicImage>
+<h:graphicImage alt="blank" url="/images/blank.gif" width="8" height="15"></h:graphicImage>
+<h:panelGroup rendered="#{Mailtool.num_files == 0}"><h:commandLink action="#{Mailtool.toggle_attachClicked}"><h:outputText value="Attach a file" /></h:commandLink></h:panelGroup>
+<h:panelGroup rendered="#{Mailtool.num_files != 0}"><h:commandLink action="#{Mailtool.toggle_attachClicked}"><h:outputText value="Attach another file" /></h:commandLink></h:panelGroup>
+<f:verbatim><br/></f:verbatim>
+</h:panelGroup>
+
+<h:dataTable value="#{Mailtool.allAttachments}" var="attachment">
+<h:column><h:graphicImage alt="blank" url="/images/blank.gif" width="26" height="15"></h:graphicImage></h:column>
+<h:column><h:outputText style="font-weight:bold;" value="#{attachment.filename} "/></h:column>
+<h:column><h:outputText value="(#{attachment.size/1000} KB)" /></h:column>
+<h:column><h:commandLink action="#{Mailtool.processRemoveFile}"><h:outputText value="remove" />
+<f:param name="id" value="#{attachment.filename}" />
+</h:commandLink>
+</h:column>
+</h:dataTable>
+
+<h:panelGroup rendered="#{Mailtool.allowedToSend}" >
+<h:outputText value="#{msgs.message_body}"/>
+</h:panelGroup>
+
+<h:panelGroup rendered="#{Mailtool.allowedToSend}">
+<sakai:rich_text_area  rows="10" columns="70" value="#{Mailtool.messageBody}"/>
+</h:panelGroup>
+<BR/>
+<h:selectBooleanCheckbox rendered="#{Mailtool.emailArchived and Mailtool.allowedToSend}" value="#{Mailtool.archiveMessage}" />
+<h:outputText rendered="#{Mailtool.emailArchived and Mailtool.allowedToSend}" value="#{msgs.append_to_email_archive}" />
+<br/>
+
+<sakai:button_bar>
+		<sakai:button_bar_item action="#{Mailtool.processSendEmail}" value="#{msgs.send_mail_button}" rendered="#{Mailtool.allowedToSend}" immediate="false" />
+		<sakai:button_bar_item action="#{Mailtool.processCancelEmail}" value="#{msgs.cancel_mail_button}" rendered="#{Mailtool.allowedToSend}" immediate="false" />
+</sakai:button_bar>
+<%---
+		<br/>
+		<h:commandLink action="main_twopage">
+			<f:verbatim>Two page version</f:verbatim>
+		</h:commandLink>
+---%>
+</sakai:view_content>
+
+</h:form>
+</div>
+
+</body>
+</html>
+</f:view>
