c27503452bde19c4b884514725c301584a43b8ef
kimsooil@bu.edu <kimsooil@bu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-11-09 16:04:05 +0000

    Moved remotely
    git-svn-id: https://source.sakaiproject.org/svn/mailtool/tags/2.2@25802 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- bumailtool5/src/webapp/call_htmlarea-header.js
+++
@@ -1,4 +0,0 @@
-var _editor_url = "/mailtool/htmlarea3/htmlarea/";
-var _editor_lang = "en";
-
-document.write("<table class=\"groove\" border=\"1\" BORDERCOLOR=\"#d3d3d3\" cellpadding=\"0\" cellspacing=\"0\"><tr><td>");
